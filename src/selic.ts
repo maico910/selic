4096c36342d628ce958745d807ceef35609794c8
Michael Wu <maico910@gmail.com>
2021-10-30 15:54:11 -0300

    make calculation method into private scope

--- src/selic.ts
+++ src/selic.ts
@@ -59,12 +59,12 @@ export class Selic {
     return this.calculatePoupancaFromSelic(selic);
   }
 
-  calculateCdiFromSelic(selic = 0) {
+  private calculateCdiFromSelic(selic = 0) {
     const cdi = selic - this.cdiScore;
     return Number(Number(cdi).toFixed(2));
   }
 
-  calculatePoupancaFromSelic(selic = 0) {
+  private calculatePoupancaFromSelic(selic = 0) {
     const poupanca = (selic / 100) * this.poupancaPercent;
     return Number(Number(poupanca).toFixed(2));
   }
