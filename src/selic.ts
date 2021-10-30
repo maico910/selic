fc41c31717abee3f732bc6697493fc0bdef6cb61
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2021-10-30 18:12:32 -0300

    fix tests, eslint and update v0.0.7

--- src/selic.ts
+++ src/selic.ts
@@ -18,7 +18,34 @@ export class Selic {
     this.poupancaPercent = poupancaPercent;
   }
 
-  async getAllRates() {
+  /**
+  * Calculate the cdi rate from selic value
+  *
+  * @param {number} selic selic rate apy for calculation
+  * @returns {number} cdi rates apy
+  */
+   private calculateCdiFromSelic(selic: number): number {
+    const cdi = selic - this.cdiScore;
+    return Number(Number(cdi).toFixed(2));
+  }
+
+  /**
+  * Calculate the poupanca rate from selic value
+  *
+  * @param {number} selic selic rate apy for calculation
+  * @returns {number} poupanca rates apy
+  */
+  private calculatePoupancaFromSelic(selic: number): number {
+    const poupanca = (selic / 100) * this.poupancaPercent;
+    return Number(Number(poupanca).toFixed(2));
+  }
+
+  /**
+  * Fetch and calculate the brazilian selic, poupanca and cdi rates apy
+  *
+  * @returns {Promise<Rate[]>} Promise with the rate list selic, cdi and poupanca
+  */
+  async getAllRates(): Promise<Rate[]> {
     const selic = await this.getSelicRate();
     const cdi = this.calculateCdiFromSelic(selic);
     const poupanca = this.calculatePoupancaFromSelic(selic);
@@ -32,9 +59,9 @@ export class Selic {
   /**
   * Fetch and calculate brazilian selic rates apy
   *
-  * @returns {Number} fetched selic rates apy
+  * @returns {Promise<number>} fetched selic rates apy
   */
-  async getSelicRate() {
+  async getSelicRate(): Promise<number> {
     const selic = await fetchCurrentSelic();
     return Number(Number(selic).toFixed(2));
   }
@@ -42,9 +69,9 @@ export class Selic {
   /**
   * Fetch and calculate cdi rate from selic value
   *
-  * @returns {Number} fetched cdi rates apy
+  * @returns {Promise<number>} fetched cdi rates apy
   */
-  async getCdiRate() {
+  async getCdiRate(): Promise<number> {
     const selic = await this.getSelicRate();
     return this.calculateCdiFromSelic(selic);
   }
@@ -52,20 +79,10 @@ export class Selic {
   /**
   * Fetch and calculate poupanca rate from selic value
   *
-  * @returns {Number} fetched poupanca rates apy
+  * @returns {Promise<number>} fetched poupanca rates apy
   */
-  async getPoupancaRate() {
+  async getPoupancaRate(): Promise<number> {
     const selic = await this.getSelicRate();
     return this.calculatePoupancaFromSelic(selic);
-  }
-
-  private calculateCdiFromSelic(selic = 0) {
-    const cdi = selic - this.cdiScore;
-    return Number(Number(cdi).toFixed(2));
-  }
-
-  private calculatePoupancaFromSelic(selic = 0) {
-    const poupanca = (selic / 100) * this.poupancaPercent;
-    return Number(Number(poupanca).toFixed(2));
   }
 }
