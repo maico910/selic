d0fa036670a13f7f9ab44986fad485ac7ad5ac82
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2021-11-01 18:54:30 -0300

    added IPCA rate

--- test/selic.spec.ts
+++ test/selic.spec.ts
@@ -1,50 +1,193 @@
-import { fetchCurrentSelic } from './bcb'
-import { fetchCurrentCdi } from './cetip'
-import { RatesObject, RatesList } from './types'
-import { SELIC, CDI } from './constants'
+import * as nock from 'nock'
+import {
+  SELIC, CDI, IPCA,
+  BCB_API, BCB_SELIC_PATH, BCB_IPCA_PATH,
+  CETIP_API, CETIP_CDI_PATH
+} from '../src/constants'
+import {
+  getRatesObject, getRatesList,
+  getSelicRate, getCdiRate, getIpcaRate
+} from '../src/selic'
 
-/**
-  * Fetch brazilian selic and cdi rates apy in object
-  *
-  * @returns {Promise<RatesObject>} Promise with rates in object
-  */
-export async function getRatesObject(): Promise<RatesObject> {
-  const [selic, cdi] = await Promise.all([
-    getSelicRate(),
-    getCdiRate(),
-  ]);
-  return { selic, cdi };
-}
+let bcbSelicNock;
+let bcbIpcaNock;
+let cetipCdiNock;
 
-/**
-  * Fetch brazilian selic and cdi rates apy in array
-  *
-  * @returns {Promise<RatesList[]>} Promise with rates in array
-  */
-export async function getRatesList(): Promise<RatesList[]> {
-  const { selic, cdi } = await getRatesObject();
-  return [
-    { name: SELIC, apy: selic },
-    { name: CDI, apy: cdi },
-  ];
-}
+describe('Selic', () => {
+  beforeEach(() => {
+    bcbSelicNock = nock(BCB_API).get(BCB_SELIC_PATH);
+    bcbIpcaNock = nock(BCB_API).get(BCB_IPCA_PATH);
+    cetipCdiNock = nock(CETIP_API).get(CETIP_CDI_PATH);
+  });
 
-/**
-  * Fetch brazilian selic rates apy
-  *
-  * @returns {Promise<number>} fetched selic rates apy
-  */
-export async function getSelicRate(): Promise<number> {
-  const selic = await fetchCurrentSelic();
-  return selic;
-}
+  afterEach(() => {
+    bcbSelicNock = null;
+    cetipCdiNock = null;
+  });
 
-/**
-  * Fetch cdi rate apy
-  *
-  * @returns {Promise<number>} fetched cdi rates apy
-  */
-export async function getCdiRate(): Promise<number> {
-  const cdi = await fetchCurrentCdi();
-  return cdi;
-}
+  describe('.getRatesObject', () => {
+    test('returns the selic, poupanca and cdi rates', async () => {
+      const fakeSelic = 10.11;
+      const fakeCdi = 11.11;
+      const fakeIpca = 11.11;
+      const fakeDataSelic = JSON.stringify({ conteudo: [{ MetaSelic: fakeSelic }]});
+      const fakeDataCdi = JSON.stringify({ taxa: fakeCdi.toString().replace('.', ',') });
+      const fakeDataIpca = JSON.stringify({ conteudo: [{ taxaInflacao: fakeIpca }]});
+
+      bcbSelicNock.reply(200, fakeDataSelic);
+      bcbIpcaNock.reply(200, fakeDataIpca);
+      cetipCdiNock.reply(200, fakeDataCdi);
+
+      const rates = await getRatesObject();
+      const expectedSelic = fakeSelic;
+      const expectedCdi = fakeCdi;
+      const expectedIpca = fakeIpca;
+
+      expect(rates).toStrictEqual({ selic: expectedSelic, cdi: expectedCdi, ipca: expectedIpca });
+    });
+
+    test('raises error when bcb and cetip scraps parse fail', async () => {
+      const fakeData = '<>INVALID</>';
+
+      bcbSelicNock.reply(200, fakeData);
+      bcbIpcaNock.reply(200, fakeData);
+      cetipCdiNock.reply(200, fakeData);
+
+      await expect(getRatesObject()).rejects.toThrow('Parse error');
+    });
+
+    test('raises error when bcb and cetip scraps fail', async () => {
+      bcbSelicNock.replyWithError('Api fails');
+      bcbIpcaNock.replyWithError('Api fails');
+      cetipCdiNock.replyWithError('Api fails');
+
+      await expect(getRatesObject()).rejects.toThrow('Request error');
+    });
+  });
+
+  describe('.getRatesList', () => {
+    test('returns the selic, poupanca and cdi rates', async () => {
+      const fakeSelic = 10.11;
+      const fakeCdi = 11.11;
+      const fakeIpca = 13.44;
+      const fakeDataSelic = JSON.stringify({ conteudo: [{ MetaSelic: fakeSelic }]});
+      const fakeDataCdi = JSON.stringify({ taxa: fakeCdi.toString().replace('.', ',') });
+      const fakeDataIpca = JSON.stringify({ conteudo: [{ taxaInflacao: fakeIpca }]});
+
+      bcbSelicNock.reply(200, fakeDataSelic);
+      bcbIpcaNock.reply(200, fakeDataIpca);
+      cetipCdiNock.reply(200, fakeDataCdi);
+
+      const rates = await getRatesList();
+      const expectedSelic = fakeSelic;
+      const expectedCdi = fakeCdi;
+      const expectedIpca = fakeIpca;
+
+      expect(rates[0]).toStrictEqual({ name: SELIC, apy: expectedSelic });
+      expect(rates[1]).toStrictEqual({ name: CDI, apy: expectedCdi });
+      expect(rates[2]).toStrictEqual({ name: IPCA, apy: expectedIpca });
+    });
+
+    test('raises error when bcb and cetip scraps parse fail', async () => {
+      const fakeData = '<>INVALID</>';
+
+      bcbSelicNock.reply(200, fakeData);
+      bcbIpcaNock.reply(200, fakeData);
+      cetipCdiNock.reply(200, fakeData);
+
+      await expect(getRatesList()).rejects.toThrow('Parse error');
+    });
+
+    test('raises error when bcb and cetip scraps fail', async () => {
+      bcbSelicNock.replyWithError('Api fails');
+      bcbIpcaNock.replyWithError('Api fails');
+      cetipCdiNock.replyWithError('Api fails');
+
+      await expect(getRatesList()).rejects.toThrow('Request error');
+    });
+  });
+
+  describe('.getSelicRate', () => {
+    test('returns the selic rates when bcb scraps succeed', async () => {
+      const fakeSelic = 10.11;
+      const fakeData = JSON.stringify({ conteudo: [{ MetaSelic: fakeSelic }]})
+
+      bcbSelicNock.reply(200, fakeData);
+
+      const value = await getSelicRate();
+      const expected = fakeSelic;
+
+      expect(value).toBe(expected);
+    });
+
+    test('raises error when bcb scraps parse fail', async () => {
+      const fakeData = '<>INVALID</>';
+
+      bcbSelicNock.reply(200, fakeData);
+
+      await expect(getSelicRate()).rejects.toThrow('Parse error');
+    });
+
+    test('raises error when bcb scraps fail', async () => {
+      bcbSelicNock.replyWithError('Api fails');
+
+      await expect(getSelicRate()).rejects.toThrow('Request error');
+    });
+  });
+
+  describe('.getIpcaRate', () => {
+    test('returns the ipca rate when bcb scraps succeed', async () => {
+      const fakeIpca = 13.11;
+      const fakeData = JSON.stringify({ conteudo: [{ taxaInflacao: fakeIpca }]})
+
+      bcbIpcaNock.reply(200, fakeData);
+
+      const value = await getIpcaRate();
+      const expected = fakeIpca;
+
+      expect(value).toBe(expected);
+    });
+
+    test('raises error when bcb scraps parse fail', async () => {
+      const fakeData = '<>INVALID</>';
+
+      bcbIpcaNock.reply(200, fakeData);
+
+      await expect(getIpcaRate()).rejects.toThrow('Parse error');
+    });
+
+    test('raises error when bcb scraps fail', async () => {
+      bcbIpcaNock.replyWithError('Api fails');
+
+      await expect(getIpcaRate()).rejects.toThrow('Request error');
+    });
+  });
+
+  describe('.getCdiRate', () => {
+    test('returns the cdi rates when cetip scraps succeed', async () => {
+      const fakeCdi = 9.11;
+      const fakeData = JSON.stringify({ taxa: fakeCdi.toString().replace('.', ',') });
+
+      cetipCdiNock.reply(200, fakeData);
+
+      const value = await getCdiRate();
+      const expected = fakeCdi;
+
+      expect(value).toBe(expected);
+    });
+
+    test('raises error when cetip scraps parse fail', async () => {
+      const fakeData = '<>INVALID</>';
+
+      cetipCdiNock.reply(200, fakeData);
+
+      await expect(getCdiRate()).rejects.toThrow('Parse error');
+    });
+
+    test('raises error when cetip scraps fail', async () => {
+      cetipCdiNock.replyWithError('Api fails');
+
+      await expect(getCdiRate()).rejects.toThrow('Request error');
+    });
+  });
+});
